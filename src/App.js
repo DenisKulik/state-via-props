1758545d0d4f917152d39a837b909f9a44cb4f29
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-06 17:06:07 +0700

    add state with counter click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Button from './components/Button';
 
 function App() {
+    const [ count, setCount ] = useState(0);
+
     return (<div className="App">
         <Counter/>
         <Button/>
