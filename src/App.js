7465051dd67cf0ba071cc2d9772a9e125ed60397
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-07 16:25:45 +0700

    add values for the buttons

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Counter from './components/Counter';
 import Button from './components/Button';
 
+const texts = [ 'Click me!', 'Click me please!', 'Hit me!', 'Press me!' ];
+
 function App() {
     const [ count, setCount ] = useState(0);
     const incrementCount = () => setCount(count + 1);
 
     return (<div className="App">
         <Counter count={count}/>
-        <Button onClick={incrementCount}/>
-        <Button onClick={incrementCount}/>
-        <Button onClick={incrementCount}/>
-        <Button onClick={incrementCount}/>
+        <Button onClick={incrementCount} text={texts[0]}/>
+        <Button onClick={incrementCount} text={texts[1]}/>
+        <Button onClick={incrementCount} text={texts[2]}/>
+        <Button onClick={incrementCount} text={texts[3]}/>
     </div>);
 }
 
